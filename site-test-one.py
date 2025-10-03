6241926fc73b5f8f6ec5d1955f23c1c6ce8bca83
Betha-nia <58006139+Betha-nia@users.noreply.github.com>
2025-10-03 16:26:47 -0300

    Add files via upload

--- site-test-one.py
+++ site-test-one.py
@@ -1,4 +1,39 @@
 import streamlit as st
 
-st.title("Knife edge calculator - USL")
-st.write("Welcome!")
+st.set_page_config(page_title="Ultrafast Spectroscopy Laser Calculator", layout="wide")
+
+#Title of the site
+st.title("Data analysis panel")
+
+#Creating the tabs
+tab1,tab2,tab3,tab4 = st.tabs(["About us","Knife Edge","Photon flux","Exciton per dot"])
+
+#Tab1: About us
+with tab1:
+    st.header("About the group")
+    st.write("""
+        Somos um grupo dedicado à análise de dados científicos, com foco em processos ópticos e fotônicos.
+        Este painel foi criado para facilitar nossos cálculos e visualizações de forma colaborativa.
+    """)
+
+#Tab2: Knife Edge
+with tab1:
+    st.header("Knife edge")
+    st.write("""
+        Knife edge calculator
+    """)
+
+#Tab3: Photon flux
+with tab1:
+    st.header("Photon flux")
+    st.write("""
+        Set the wavelength and the spot size
+    """)
+
+#Tab4: Exciton per dot
+with tab1:
+    st.header("Exciton per dot")
+    st.write("""
+        Set wavelength
+    """)
+
